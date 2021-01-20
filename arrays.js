f4aba4267eb9af8486549798a65e898e2fb81150
tbcwebsitecreation <rubinaramesh1973@gmail.com>
2021-01-20 09:20:16 -0700

    completed variable

---
+++ arrays.js
@@ -0,0 +1,55 @@
+/*
+ * Work through each of the prompts below. Update `index.html` with the link to
+ * this file to see your results in the console.
+ *
+ * Pro Tip: Comment out your solution to a prompt before moving on to the next
+ * one.
+ */
+// alert("Hello")
+/*
+ * Prompt 01:
+ *
+ * Create a variable called message using var and assign it the value 'Hello
+ * World'
+ */
+var message = 'Hello World';
+console.log(message);
+/*
+ * Prompt 02:
+ *
+ *  Create a variable called firstName using let and assign it the value of your
+ *  first name.
+ */
+let firstName = 'Rubina';
+console.log(firstName);
+/*
+ * Prompt 03:
+ *
+ * Create a variable called lastName using const and assign it the value of your
+ * last name.
+ */
+let lastName = 'Ramesh';
+console.log(lastName);
+/*
+ * Prompt 04:
+ *
+ * Create a variable called number and assign a number to it. On separate lines
+ * for each, assign a string, another number, and a boolean value to the
+ * variable.
+ *
+ * What do we need to use to define variables we can change?
+ */
+let num = 9;
+num = 'Hello';
+num = 22;
+num = false;
+console.log(num);
+/*
+ * Prompt 05:
+ *
+ * Define a variable in such a way that we can't change it's value later.
+ */
+const noChange = 'Hello';
+console.log(noChange);
+noChange = 'World';
+console.log(noChange);
---
+++ variables.js
@@ -0,0 +1,55 @@
+/*
+ * Work through each of the prompts below. Update `index.html` with the link to
+ * this file to see your results in the console.
+ *
+ * Pro Tip: Comment out your solution to a prompt before moving on to the next
+ * one.
+ */
+
+/*
+ * Prompt 01:
+ *
+ * Create a variable called message using var and assign it the value 'Hello
+ * World'
+ */
+let message = "Hello World";
+console.log(message);
+/*
+ * Prompt 02:
+ *
+ *  Create a variable called firstName using let and assign it the value of your
+ *  first name.
+ */
+let firstName = "ritvik";
+console.log(firstName);
+/*
+ * Prompt 03:
+ *
+ * Create a variable called lastName using const and assign it the value of your
+ * last name.
+ */
+let lastName = "ramesh";
+console.log(lastName);
+/*
+ * Prompt 04:
+ *
+ * Create a variable called number and assign a number to it. On separate lines
+ * for each, assign a string, another number, and a boolean value to the
+ * variable.
+ *
+ * What do we need to use to define variables we can change?
+ */
+let num = 9;
+num = "hello";
+num = 22;
+num = false;
+console.log(num);
+/*
+ * Prompt 05:
+ *
+ * Define a variable in such a way that we can't change it's value later.
+ */
+const str = "Hello";
+console.log(str);
+str = " world "
+console.log(str);
